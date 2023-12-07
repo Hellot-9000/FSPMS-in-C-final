4d382aba883107224ee4a99040299ffe91a9cd20
Hellot-9000 <119919411+Hellot-9000@users.noreply.github.com>
2023-12-07 02:27:58 -0700

    Update main.c BETA

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+// Beta version includes reservation management
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,10 +6,20 @@
 
 #define MAX_STRING_LENGTH 50
 #define MAX_ACCOUNTS 100
+#define MAX_RESERVATIONS 100
 
 struct Account {
     char username[MAX_STRING_LENGTH];
     char password[MAX_STRING_LENGTH];
+};
+
+struct Reservation {
+    int reservationNumber;
+    char guestName[MAX_STRING_LENGTH];
+    char checkInDate[MAX_STRING_LENGTH];
+    char checkOutDate[MAX_STRING_LENGTH];
+    int roomNumber;
+    char status[MAX_STRING_LENGTH];
 };
 
 void displayInvalidOptionAndClearScreen() {
@@ -18,25 +29,27 @@ void displayInvalidOptionAndClearScreen() {
 
 void login(struct Account *accounts, int *numAccounts, int *loggedInUser);
 void addAccount(struct Account *accounts, int *numAccounts);
+void displayMainMenu(struct Reservation *reservations, int *numReservations);
+void addReservation(struct Reservation *reservations, int *numReservations);
 
 int main() {
     struct Account *accounts = malloc(MAX_ACCOUNTS * sizeof(struct Account));
+    struct Reservation *reservations = malloc(MAX_RESERVATIONS * sizeof(struct Reservation));
 
-    if (accounts == NULL) {
+    if (accounts == NULL || reservations == NULL) {
         perror("Error allocating memory");
         return 1;
     }
 
     int numAccounts = 0;
+    int numReservations = 0;
     int loggedInUser = 0;
 
     do {
         login(accounts, &numAccounts, &loggedInUser);
 
         if (loggedInUser) {
-            // Your alpha version logic goes here
-            printf("Welcome, %s!\n", accounts[loggedInUser - 1].username);
-
+            displayMainMenu(reservations, &numReservations);
             // This is just a placeholder. You need to implement the main menu logic.
             // For now, let's break the loop.
             loggedInUser = 0;
@@ -46,52 +59,46 @@ int main() {
 
     // Free allocated memory before exiting
     free(accounts);
+    free(reservations);
 
     return 0;
 }
 
-void login(struct Account *accounts, int *numAccounts, int *loggedInUser) {
-    char inputUsername[MAX_STRING_LENGTH];
-    char inputPassword[MAX_STRING_LENGTH];
+void displayMainMenu(struct Reservation *reservations, int *numReservations) {
+    int selectedMenu;
 
-    printf("Enter username: ");
-    scanf("%s", inputUsername);
+    do {
+        printf("\nMain Menu\n");
+        printf("1. Add Reservation\n");
+        printf("2. View Reservations\n");
+        printf("0. Log Off\n");
+        printf("Enter your choice: ");
+        scanf("%d", &selectedMenu);
 
-    printf("Enter password: ");
-    scanf("%s", inputPassword);
-
-    for (int i = 0; i < *numAccounts; ++i) {
-        if (strcmp(inputUsername, accounts[i].username) == 0 && strcmp(inputPassword, accounts[i].password) == 0) {
-            *loggedInUser = i + 1;
-            printf("Login successful!\n");
-            return;
+        switch (selectedMenu) {
+            case 1:
+                addReservation(reservations, numReservations);
+                break;
+            case 2:
+                // Your logic to view reservations goes here
+                break;
+            case 0:
+                printf("Logging off...\n");
+                break;
+            default:
+                displayInvalidOptionAndClearScreen();
         }
-    }
 
-    printf("Invalid username or password. Please try again.\n");
-    *loggedInUser = 0;
+    } while (selectedMenu != 0);
 }
 
-void addAccount(struct Account *accounts, int *numAccounts) {
-    if (*numAccounts >= MAX_ACCOUNTS) {
-        printf("Error: Maximum number of accounts reached.\n");
+void addReservation(struct Reservation *reservations, int *numReservations) {
+    if (*numReservations >= MAX_RESERVATIONS) {
+        printf("Error: Maximum number of reservations reached.\n");
         return;
     }
 
-    printf("Enter new username: ");
-    scanf("%s", accounts[*numAccounts].username);
+    // Your logic to add a reservation goes here
 
-    // Check if the username already exists
-    for (int i = 0; i < *numAccounts; ++i) {
-        if (strcmp(accounts[i].username, accounts[*numAccounts].username) == 0) {
-            printf("Error: Username already exists.\n");
-            return;
-        }
-    }
-
-    printf("Enter new password: ");
-    scanf("%s", accounts[*numAccounts].password);
-
-    (*numAccounts)++;
-    printf("Account created successfully.\n");
+    printf("Reservation added successfully.\n");
 }
